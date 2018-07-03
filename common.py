9a4096cc4062cf67b8c6715fe20666bccdf3585e
chriscohoat <chris.cohoat@gmail.com>
2018-07-03 16:55:50 -0600

    Added base

--- common.py
+++ common.py
@@ -2,18 +2,21 @@ import glob
 import logging
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient
 from config.settings import IOT_CLIENT_ID, IOT_ENDPOINT
+import os
+
+BASE_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
 
 def build_shadow_client():
-    ca_path = glob.glob('config/*Authority*.pem')
-    key_path = glob.glob('config/*private.pem.key')
-    cert_path = glob.glob('config/*certificate.pem.crt')
-    if not ca_path or not key_path or not cert_path:
+    certificate_authority = glob.glob(os.path.join(BASE_DIRECTORY, 'config', '*Authority*.pem'))
+    private_key = glob.glob(os.path.join(BASE_DIRECTORY, 'config', '*private.pem.key'))
+    certificate = glob.glob(os.path.join(BASE_DIRECTORY, 'config', '*certificate.pem.crt'))
+    if not certificate_authority or not private_key or not certificate:
         logging.warning('Need CA, private key, and certificate file located in config/')
         return None
     shadow_client = AWSIoTMQTTShadowClient(IOT_CLIENT_ID)
     shadow_client.configureEndpoint(IOT_ENDPOINT, 8883)
-    shadow_client.configureCredentials(ca_path[0], key_path[0], cert_path[0])
+    shadow_client.configureCredentials(certificate_authority[0], private_key[0], certificate[0])
     shadow_client.configureConnectDisconnectTimeout(10)
     shadow_client.configureMQTTOperationTimeout(5)
     return shadow_client
