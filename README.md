03730fdc77455ba6a4f1c9deaeae21c4f29b2e7c
chriscohoat <chris.cohoat@gmail.com>
2018-07-03 08:21:01 -0600

    Improved README

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
+# Instructions
+
+1) Copy `config/settings.example.py` to `config/settings.py`
+
+2) Download your Thing's certificates (private, cert, and CA) and put them in config
+
+3) Run `main.py`
+
+# To copy certs to a Raspberry Pi using SCP
+
+```
+export SCP_LOCATION=pi@192.168.86.29:~/ambient-sensor/config/
+scp './config/\*certificate.pem.crt' $SCP_LOCATION
+```
+
 # Resize example
 
 https://github.com/jquast/blessed/blob/master/bin/on_resize.py
--- main.py
+++ main.py
@@ -1,23 +1,86 @@
 from Adafruit_SHT31 import *
-from blessings import Terminal
 import time
+import logging
+import glob
 
-sensor = SHT31(address=0x44)
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient, AWSIoTMQTTShadowClient
+from config.settings import IOT_CLIENT_ID, IOT_ENDPOINT
+
+logging.basicConfig(level=logging.INFO)
+
+ca_path = glob.glob('config/*Authority*.pem')
+key_path = glob.glob('config/*private.pem.key')
+cert_path = glob.glob('config/*certificate.pem.crt')
+
+
+def build_shadow_client():
+    myShadowClient = AWSIoTMQTTShadowClient(IOT_CLIENT_ID)
+    # For Websocket connection
+    # myMQTTClient = AWSIoTMQTTClient("myClientID", useWebsocket=True)
+    # Configurations
+    # For TLS mutual authentication
+    myShadowClient.configureEndpoint(IOT_ENDPOINT, 8883)
+    # For Websocket
+    # myShadowClient.configureEndpoint("YOUR.ENDPOINT", 443)
+    # For TLS mutual authentication with TLS ALPN extension
+    # myShadowClient.configureEndpoint("YOUR.ENDPOINT", 443)
+    myShadowClient.configureCredentials(ca_path[0], key_path[0], cert_path[0])
+    # For Websocket, we only need to configure the root CA
+    # myShadowClient.configureCredentials("YOUR/ROOT/CA/PATH")
+    myShadowClient.configureConnectDisconnectTimeout(10)  # 10 sec
+    myShadowClient.configureMQTTOperationTimeout(5)  # 5 sec
+    return myShadowClient
+
+
+def build_mqtt_client():
+    # For certificate based connection
+    myMQTTClient = AWSIoTMQTTClient(IOT_CLIENT_ID)
+    # For Websocket connection
+    # myMQTTClient = AWSIoTMQTTClient("myClientID", useWebsocket=True)
+    # Configurations
+    # For TLS mutual authentication
+    myMQTTClient.configureEndpoint(IOT_ENDPOINT, 8883)
+    # For Websocket
+    # myMQTTClient.configureEndpoint("YOUR.ENDPOINT", 443)
+    # For TLS mutual authentication with TLS ALPN extension
+    # myMQTTClient.configureEndpoint("YOUR.ENDPOINT", 443)
+    myMQTTClient.configureCredentials(ca_path[0], key_path[0], cert_path[0])
+    # For Websocket, we only need to configure the root CA
+    # myMQTTClient.configureCredentials("YOUR/ROOT/CA/PATH")
+    myMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+    myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+    myMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+    myMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+    return myMQTTClient
 
 
 def main():
-    degrees = sensor.read_temperature()
-    humidity = sensor.read_humidity()
-    print('Temp             = {0:0.3f} deg C'.format(degrees))
-    print('Humidity         = {0:0.2f} %'.format(humidity))
-	
-    t = Terminal()
-    with t.fullscreen():
-        print t.bold('Hi there!')
-        print t.bold_red_on_bright_green('It hurts my eyes!')
+    try:
+        sensor = SHT31(address=0x44)
+    except:
+        logging.warning('Temperature and humidity sensor cannot be loaded')
+        sensor = None
 
-        with t.location(0, t.height):
-            print 'This is at the bottom.'
+    if not ca_path or not key_path or not cert_path:
+        logging.warning('Need CA, private key, and certificate file located in config/')
+        iot_client = None
+    else:
+        logging.info('Building shadow client.')
+        iot_client = build_shadow_client()
+
+    if iot_client:
+        logging.info('Connecting to IoT')
+        iot_client.connect()
+        logging.info('Connected to IoT!')
+    else:
+        logging.warning('Unable to connect to IoT')
+
+    while True:
+        if sensor:
+            degrees = sensor.read_temperature()
+            humidity = sensor.read_humidity()
+            print('Temp             = {0:0.3f} deg C'.format(degrees))
+            print('Humidity         = {0:0.2f} %'.format(humidity))
         time.sleep(5)
 
 
